1b57a31a78daa66dc5aa526da9a80c767873e15d
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-04-04 01:40:34 -0300

    remove unnecessary import

--- src/controllers/pokestops.js
+++ src/controllers/pokestops.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const { findByIdAndUpdate } = require('../models/pokestop')
 const Pokestop = require('../models/pokestop')
 
 router.get('/', async (req, res) => {
